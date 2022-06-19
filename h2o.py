fd03fc9b1e48ece6ee35369a9561dda8ed1ddcf1
Prayas Shrestha <73539544+PrayasXtha@users.noreply.github.com>
2022-06-19 11:29:58 +0545

    Update h2o.py

--- h2o.py
+++ h2o.py
@@ -10,10 +10,10 @@ from plyer import notification
 if __name__ == "__main__":
    while True:
     notification.notify(
-        title = "Drink water!",
-        message = "For you health bro!",
-        app_icon = "C:\\Users\\DELL\\Desktop\\drink water reminder\\icon.ico",
-        timeout=12
+        title = "Drink water!", #change the notification title here
+        message = "For you health bro!", #change the message here
+        app_icon = "C:\\Users\\DELL\\Desktop\\drink water reminder\\icon.ico", #use the your icon path here
+        timeout=12 #timeout denotes the time span the meaage will pop for
 
      )
-    time.sleep(60*60)
+    time.sleep(60*60) #time sleep denotes the time interval between the nofication, here its set for every 1 hrs 1 notification
